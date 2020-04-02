f3ca076bd5aa0d59f73fe65cbbcc5b0f89f459f2
Natalia <natikrak@gmail.com>
2020-04-02 16:21:10 +0200

    mySQL app

---
+++ src/main/java/pl/natalia/accessingMySql/AccessingMySqlApplication.java
@@ -0,0 +1,22 @@
+package pl.natalia.accessingMySql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+//   JSON:
+//   {
+//
+//			"name": "Natalia",
+//			"email": "natalia@gmail.com"
+//
+//			}
+@SpringBootApplication
+public class AccessingMySqlApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AccessingMySqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/natalia/accessingMySql/AccessingMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.natalia.accessingMySql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingMySqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
